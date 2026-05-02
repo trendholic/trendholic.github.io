a7b49d0ae3f6357efb606f74604216aafa2e0645
trendholic <onlinetrader002@gmail.com>
2026-05-02 17:30:06 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+fetch("products.json")
+  .then(res => res.json())
+  .then(data => {
+    let html = "";
+
+    data.forEach(p => {
+      html += `
+        <div class="card">
+          <img src="${p.image}">
+          <h3>${p.name}</h3>
+          <p>${p.price}</p>
+          <a href="${p.link}">
+            <button>Order on WhatsApp</button>
+          </a>
+        </div>
+      `;
+    });
+
+    document.getElementById("products").innerHTML = html;
+  });
